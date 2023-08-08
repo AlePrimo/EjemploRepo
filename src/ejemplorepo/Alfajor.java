c51957ec1385c08301e364404281825b972f635c
Ale Primo <alejandrojuliancarullo@hotmail.com>
2023-08-08 19:12:29 -0300

    Initial commit

---
+++ src/ejemplorepo/Alfajor.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejemplorepo;
+
+
+public class Alfajor {
+    
+    private String sabor;
+    private int peso;
+
+    public Alfajor(String sabor, int peso) {
+        this.sabor = sabor;
+        this.peso = peso;
+    }
+
+    public String getSabor() {
+        return sabor;
+    }
+
+    public void setSabor(String sabor) {
+        this.sabor = sabor;
+    }
+
+    public int getPeso() {
+        return peso;
+    }
+
+    public void setPeso(int peso) {
+        this.peso = peso;
+    }
+    
+    
+    
+    
+    
+}
