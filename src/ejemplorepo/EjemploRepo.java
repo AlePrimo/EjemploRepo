17cc58d60036dc3f9d3b4e671232460745a06b8f
AndresNT <traversoandresn@gmail.com>
2023-08-09 18:47:18 -0300

    Objeto alfajor

--- src/ejemplorepo/EjemploRepo.java
+++ src/ejemplorepo/EjemploRepo.java
@@ -15,7 +15,9 @@ public class EjemploRepo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        Alfajor jorgito=new Alfajor("Chocolate", 300);
+        
     }
     
 }
