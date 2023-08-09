87a18911546392b24b246082ef6f342a3d3e5d73
joselesc <joselescanogm@gmail.com>
2023-08-09 18:51:42 -0300

    cambios en kiosco

--- src/ejemplorepo/Kiosco.java
+++ src/ejemplorepo/Kiosco.java
@@ -11,4 +11,38 @@ package ejemplorepo;
  */
 public class Kiosco {
     
+    private String ciudad;
+    private String direccion;
+    private String cliente;
+
+    public Kiosco(String ciudad, String direccion, String cliente) {
+        this.ciudad = ciudad;
+        this.direccion = direccion;
+        this.cliente = cliente;
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(String cliente) {
+        this.cliente = cliente;
+    }
+    
 }
